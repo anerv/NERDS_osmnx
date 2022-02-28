a5a9cf6d7fe28dfb3e9e72c0ac526883848c2456
csebastiao <clement.sebastiao@gmail.com>
2022-02-28 08:13:19 +0100

    add modified simplification pyfile, simplify functions and add attribute discrimination

---
+++ osmnx/simplification_modified.py
@@ -0,0 +1,293 @@
+# -*- coding: utf-8 -*-
+"""
+Attempt to modify the simplification functions from OSMnx.
+"""
+
+import networkx as nx
+from shapely.geometry import Point
+from shapely.geometry import LineString
+
+def _is_endpoint(G, node, attributes = None, strict=True):
+    """
+    Is node a true endpoint of an edge.
+
+    Return True if the node is a "real" endpoint of an edge in the network,
+    otherwise False. OSM data includes lots of nodes that exist only as points
+    to help streets bend around curves. An end point is a node that either:
+    1) is its own neighbor, ie, it self-loops.
+    2) or, has no incoming edges or no outgoing edges, ie, all its incident
+    edges point inward or all its incident edges point outward.
+    3) or, it does not have exactly two neighbors and degree of 2 or 4.
+    4) or, if strict mode is false, if its edges have different OSM IDs.
+
+    Parameters
+    ----------
+    G : networkx.MultiDiGraph
+        input graph
+    node : int
+        the node to examine
+    attributes : list
+        key of the attributes we should discriminate
+    strict : bool
+        if False, allow nodes to be end points even if they fail all other 
+        rules but have edges with different OSM IDs
+
+    Returns
+    -------
+    bool
+    """
+    neighbors = set(list(G.predecessors(node)) + list(G.successors(node)))
+    n = len(neighbors)
+    d = G.degree(node)
+
+    # rule 1
+    if node in neighbors:
+        # if the node appears in its list of neighbors, it self-loops
+        # this is always an endpoint.
+        return True
+
+    # rule 2
+    elif G.out_degree(node) == 0 or G.in_degree(node) == 0:
+        # if node has no incoming edges or no outgoing edges, it is an endpoint
+        return True
+
+    # rule 3
+    elif not (n == 2 and (d == 2 or d == 4)):
+        # else, if it does NOT have 2 neighbors AND either 2 or 4 directed
+        # edges, it is an endpoint. either it has 1 or 3+ neighbors, in which
+        # case it is a dead-end or an intersection of multiple streets or it 
+        # has 2 neighbors but 3 degree (indicating a change from oneway to 
+        # twoway) or more than 4 degree (indicating a parallel edge) and thus 
+        # is an endpoint
+        if not attributes is None:
+            for attr in attributes:
+                if (G.edges[list(G.predecessors(node))[0], node, 0][attr]) == (
+                        G.edges[node, list(G.successors(node))[0], 0][attr]):
+                    pass
+                else:
+                    return False
+        return True
+
+    # rule 4
+    elif not strict:
+        # non-strict mode: do its incident edges have different OSM IDs?
+        osmids = []
+
+        # add all the edge OSM IDs for incoming edges
+        for u in G.predecessors(node):
+            for key in G[u][node]:
+                osmids.append(G.edges[u, node, key]["osmid"])
+
+        # add all the edge OSM IDs for outgoing edges
+        for v in G.successors(node):
+            for key in G[node][v]:
+                osmids.append(G.edges[node, v, key]["osmid"])
+
+        # if there is more than 1 OSM ID in the list of edge OSM IDs then it is
+        # an endpoint, if not, it isn't
+        return len(set(osmids)) > 1
+
+    # if none of the preceding rules returned true, then it is not an endpoint
+    else:
+        return False
+
+
+def _build_path(G, endpoint, endpoint_successor, endpoints):
+    """
+    Build a path of nodes from one endpoint node to next endpoint node.
+
+    Parameters
+    ----------
+    G : networkx.MultiDiGraph
+        input graph
+    endpoint : int
+        the endpoint node from which to start the path
+    endpoint_successor : int
+        the successor of endpoint through which the path to the next endpoint
+        will be built
+    endpoints : set
+        the set of all nodes in the graph that are endpoints
+
+    Returns
+    -------
+    path : list
+        the first and last items in the resulting path list are endpoint
+        nodes, and all other items are interstitial nodes that can be removed
+        subsequently
+    """
+    # start building path from endpoint node through its successor
+    path = [endpoint, endpoint_successor]
+
+    # for each successor of the endpoint's successor
+    for successor in G.successors(endpoint_successor):
+        if successor not in path:
+            # if this successor is already in the path, ignore it, otherwise 
+            # add it to the path
+            path.append(successor)
+            while successor not in endpoints:
+                # find successors (of current successor) not in path
+                successors = [n for n in G.successors(successor) if n not in path]
+
+                # 99%+ of the time there will be only 1 successor: add to path
+                if len(successors) == 1:
+                    successor = successors[0]
+                    path.append(successor)
+
+                # handle relatively rare cases or OSM digitization quirks
+                elif len(successors) == 0:
+                    if endpoint in G.successors(successor):
+                        # we have come to the end of a self-looping edge, so
+                        # add first node to end of path to close it and return
+                        return path + [endpoint]
+                    else:  # pragma: no cover
+                        # this can happen due to OSM digitization error where
+                        # a one-way street turns into a two-way here, but
+                        # duplicate incoming one-way edges are present
+                        return path
+                else:  # pragma: no cover
+                    # if successor has >1 successors, then successor must have
+                    # been an endpoint because you can go in 2 new directions.
+                    # this should never occur in practice
+                    raise Exception(f"Unexpected simplify pattern failed near {successor}")
+
+            # if this successor is an endpoint, we've completed the path
+            return path
+
+    # if endpoint_successor has no successors not already in the path, return
+    # the current path: this is usually due to a digitization quirk on OSM
+    return path
+
+
+def _get_paths_to_simplify(G, attributes = None, strict=True):
+    """
+    Generate all the paths to be simplified between endpoint nodes.
+
+    The path is ordered from the first endpoint, through the interstitial 
+    nodes, to the second endpoint.
+
+    Parameters
+    ----------
+    G : networkx.MultiDiGraph
+        input graph
+    attributes : list
+        key of the attributes we should discriminate
+    strict : bool
+        if False, allow nodes to be end points even if they fail all other 
+        rules but have edges with different OSM IDs
+
+    Yields
+    ------
+    path_to_simplify : list
+    """
+    # first identify all the nodes that are endpoints
+    endpoints = set([n for n in G.nodes if _is_endpoint(G, n, 
+                                                        attributes = attributes,
+                                                        strict=strict)])
+
+    # for each endpoint node, look at each of its successor nodes
+    for endpoint in endpoints:
+        for successor in G.successors(endpoint):
+            if successor not in endpoints:
+                # if endpoint node's successor is not an endpoint, build path
+                # from the endpoint node, through the successor, and on to the
+                # next endpoint node
+                yield _build_path(G, endpoint, successor, endpoints)
+
+
+def simplify_graph(G, attributes = None, strict=True, remove_rings=True):
+    """
+    Simplify a graph's topology by removing interstitial nodes.
+
+    Simplifies graph topology by removing all nodes that are not intersections
+    or dead-ends. Create an edge directly between the end points that
+    encapsulate them, but retain the geometry of the original edges, saved as
+    a new `geometry` attribute on the new edge. Note that only simplified
+    edges receive a `geometry` attribute. Some of the resulting consolidated
+    edges may comprise multiple OSM ways, and if so, their multiple attribute
+    values are stored as a list.
+
+    Parameters
+    ----------
+    G : networkx.MultiDiGraph
+        input graph
+    strict : bool
+        if False, allow nodes to be end points even if they fail all other
+        rules but have incident edges with different OSM IDs. Lets you keep
+        nodes at elbow two-way intersections, but sometimes individual blocks
+        have multiple OSM IDs within them too.
+    remove_rings : bool
+        if True, remove isolated self-contained rings that have no endpoints
+
+    Returns
+    -------
+    G : networkx.MultiDiGraph
+        topologically simplified graph, with a new `geometry` attribute on
+        each simplified edge
+    """
+    if "simplified" in G.graph and G.graph["simplified"]:
+        raise Exception("This graph has already been simplified, cannot simplify it again.")
+
+    # make a copy to not mutate original graph object caller passed in
+    G = G.copy()
+    all_nodes_to_remove = []
+    all_edges_to_add = []
+
+    # generate each path that needs to be simplified
+    for path in _get_paths_to_simplify(G, attributes = attributes,
+                                       strict=strict):
+
+        # add the interstitial edges we're removing to a list so we can retain
+        # their spatial geometry
+        edge_attributes = dict()
+        for u, v in zip(path[:-1], path[1:]):
+
+            # there should rarely be multiple edges between interstitial nodes
+            # usually happens if OSM has duplicate ways digitized for just one
+            # street... we will keep only one of the edges (see below)
+
+            # get edge between these nodes: if multiple edges exist between
+            # them (see above), we retain only one in the simplified graph
+            edge = G.edges[u, v, 0]
+            for key in edge:
+                if key in edge_attributes:
+                    # if this key already exists in the dict, append it to the
+                    # value list
+                    edge_attributes[key].append(edge[key])
+                else:
+                    # if this key doesn't already exist, set the value to a list
+                    # containing the one value
+                    edge_attributes[key] = [edge[key]]
+        # construct the geometry and sum the lengths of the segments
+        edge_attributes["geometry"] = LineString(
+            [Point((G.nodes[node]["x"], G.nodes[node]["y"])) for node in path]
+        )
+        edge_attributes["length"] = sum(edge_attributes["length"])
+
+        # add the nodes and edges to their lists for processing at the end
+        all_nodes_to_remove.extend(path[1:-1])
+        all_edges_to_add.append(
+            {"origin": path[0], "destination": path[-1],
+             "attr_dict": edge_attributes}
+        )
+
+    # for each edge to add in the list we assembled, create a new edge between
+    # the origin and destination
+    for edge in all_edges_to_add:
+        G.add_edge(edge["origin"], edge["destination"], **edge["attr_dict"])
+
+    # finally remove all the interstitial nodes between the new edges
+    G.remove_nodes_from(set(all_nodes_to_remove))
+
+    if remove_rings:
+        # remove any connected components that form a self-contained ring
+        # without any endpoints
+        wccs = nx.weakly_connected_components(G)
+        nodes_in_rings = set()
+        for wcc in wccs:
+            if not any(_is_endpoint(G, n) for n in wcc):
+                nodes_in_rings.update(wcc)
+        G.remove_nodes_from(nodes_in_rings)
+
+    # mark graph as having been simplified
+    G.graph["simplified"] = True
+    return G
