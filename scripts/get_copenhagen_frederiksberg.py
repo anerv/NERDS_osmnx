1e180e20568b38dc261385f3d70f5bf08373f232
csebastiao <clement.sebastiao@gmail.com>
2022-03-04 11:43:07 +0100

    add fill option to get Copenhagen

--- scripts/get_copenhagen_frederiksberg.py
+++ scripts/get_copenhagen_frederiksberg.py
@@ -3,7 +3,6 @@
 
 """
 
-import geopandas as gpd
 import shapely
 import osmnx as ox
 
@@ -12,7 +11,19 @@ if __name__ == "__main__":
     fre = ox.geocode_to_gdf("Frederiksberg Municipality")
     metro_poly = shapely.ops.unary_union([cop['geometry'][0],
                                           fre['geometry'][0]])
+    # we can also fill the copenhagen polygon, works too but don't know
+    # if that is way more obvious and easy to read
+    # metro_poly = shapely.ops.unary_union([
+    #     shapely.geometry.Polygon(
+    #         list(cop['geometry'][0][0].exterior.coords)
+    #         ),
+    #     cop['geometry'][0][1:]
+    #     ])
+    
     metro = ox.graph_from_polygon(metro_poly, simplify = False)
     ox.plot_graph(metro, figsize = (12, 8), bgcolor = 'w', 
                   node_color = 'black', node_size = 30, edge_color = 'r', 
                   edge_linewidth = 3)
+    
+
+    
